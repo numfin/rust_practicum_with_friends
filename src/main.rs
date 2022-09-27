62a604f4ca4f64667dfe19d8ab133a710ce9af24
A coder <numfin@users.noreply.github.com>
2022-09-27 23:49:08 +0300

    async multithread concepts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
+use practicum::server::create_server;
 use practicum::todo::TodoList;
 
-fn main() {
-    let mut todo_list = TodoList::new();
-    let todo_id = todo_list.add_todo("asd");
-    let _todo_second_id = todo_list.add_todo("new task");
-    println!("{:#?}", todo_list.filter_todos(true));
-    todo_list.toggle_todo(&todo_id);
-    println!("{:#?}", todo_list.filter_todos(false));
-    todo_list.delete_todo(&todo_id);
-    todo_list.delete_todo(&todo_id);
-    todo_list.delete_todo("undefined_id");
-    println!("{:#?}", todo_list.todos);
+#[tokio::main]
+async fn main() {
+    create_server().await;
 }
